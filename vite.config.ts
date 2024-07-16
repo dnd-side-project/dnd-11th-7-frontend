fc2ecfd3237fc7625c71e85eb0ad1af7d835e3b6
sungjihyun <jhsung23@gmail.com>
2024-07-16 23:33:58 +0900

    set: 절대 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
+// eslint-disable-next-line no-restricted-exports
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 });
