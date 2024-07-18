0ec47e5bad487e4e3c90b1acc053197fb0c5a78b
sungjihyun <jhsung23@gmail.com>
2024-07-18 13:32:02 +0900

    Set: axios, msw

---
+++ src/lib/axios.ts
@@ -0,0 +1,31 @@
+import axios, { type AxiosResponse } from 'axios';
+
+import { ENV } from './env';
+
+export const instance = axios.create({
+  baseURL: ENV.API_BASE_URL,
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const get = <T>(...args: Parameters<typeof instance.get>) => {
+  return instance.get<T, AxiosResponse<T>>(...args);
+};
+
+export const post = <T>(...args: Parameters<typeof instance.post>) => {
+  return instance.post<T, AxiosResponse<T>>(...args);
+};
+
+export const put = <T>(...args: Parameters<typeof instance.put>) => {
+  return instance.put<T, AxiosResponse<T>>(...args);
+};
+
+export const patch = <T>(...args: Parameters<typeof instance.patch>) => {
+  return instance.patch<T, AxiosResponse<T>>(...args);
+};
+
+export const del = <T>(...args: Parameters<typeof instance.delete>) => {
+  return instance.delete<T, AxiosResponse<T>>(...args);
+};
