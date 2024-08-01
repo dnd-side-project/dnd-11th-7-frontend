c11d9bd3e2dfe449814d27612f97496407d7e2ff
Sung Ji Hyun <jhsung23@gmail.com>
2024-08-02 00:15:58 +0900

    [FEAT] global color palette, 스토리북 (#13)

---
+++ src/styles/global.ts
@@ -0,0 +1,21 @@
+export const colors = {
+  BK: '#191919',
+  GY1: '#404040',
+  GY2: '#676767',
+  GY3: '#717171',
+  GY4: '#AAAAAA',
+  GY5: '#D9D9D9',
+  GY6: '#F6F6F6',
+  WH: '#FEFEFE',
+  purple: '#857BFF',
+  P1: '#A8A1FF',
+  P2: '#DDDAFF',
+  P3: '#F3F2FF',
+  yellow: '#FFF279',
+};
+
+export const theme = {
+  colors,
+};
+
+export type Colors = keyof typeof colors;
