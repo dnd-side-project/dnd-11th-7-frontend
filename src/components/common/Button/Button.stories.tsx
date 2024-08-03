6caf5b22a0d3bb93d308f4003c1af5889b67c589
keemsebeen <sebin0580@gmail.com>
2024-08-04 00:06:32 +0900

    feat: Button 스토리북 추가

--- src/components/common/Button/Button.stories.tsx
+++ src/components/common/Button/Button.stories.tsx
@@ -2,6 +2,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import { Button } from '.';
+import { FlexBox } from '../FlexBox';
 
 const meta = {
   title: 'components/common/Button',
@@ -13,5 +14,14 @@ export default meta;
 type Story = StoryObj<typeof Button>;
 
 export const Basic: Story = {
-  render: () => <Button />,
+  render: () => (
+    <FlexBox flexDir="column" gap={8} alignItem="center">
+      <Button variant="primary" width="full">
+        버튼
+      </Button>
+      <Button variant="primary" width="full" disabled>
+        버튼
+      </Button>
+    </FlexBox>
+  ),
 };
