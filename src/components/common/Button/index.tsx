44b4efbdf8c5c4e17059e7cf700b056e8ff1c4fb
keemsebeen <sebin0580@gmail.com>
2024-08-04 00:09:50 +0900

    feat : Button 컴포넌트 구현

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,3 +1,13 @@
-export const Button = () => {
-  return <button>임시 버튼</button>;
-};
+// Button.tsx
+import { forwardRef } from 'react';
+
+import { StyledButton } from './Button.styled';
+import { Props } from './Button.types';
+
+export const Button = forwardRef<HTMLButtonElement, Props>(({ children, ...props }, ref) => {
+  return (
+    <StyledButton ref={ref} {...props}>
+      {children}
+    </StyledButton>
+  );
+});
