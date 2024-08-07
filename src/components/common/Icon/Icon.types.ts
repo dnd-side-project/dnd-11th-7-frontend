2277d51bc5f62cd6ba8acefd28090c8abfb805c9
sungjihyun <jhsung23@gmail.com>
2024-08-08 01:22:55 +0900

    feat: Icon 컴포넌트, 스토리북

---
+++ src/components/common/Icon/Icon.types.ts
@@ -0,0 +1,15 @@
+import { IconName } from '@/assets/icons';
+import { Colors } from '@/styles/global';
+
+export type Props = {
+  name: IconName;
+  /**
+   * @default 'BK'
+   */
+  color?: Colors;
+  /**
+   * @description px 단위로 변환합니다.
+   * @default 20
+   */
+  size?: number;
+} & React.SVGProps<SVGSVGElement>;
