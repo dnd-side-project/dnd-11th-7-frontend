8f7052fb89ac0c2f77f9bb352971f3b680f2fcba
keemsebeen <sebin0580@gmail.com>
2024-08-09 01:22:43 +0900

    fix: FixedBottomButton 수정

--- src/components/common/FixedBottomButton/FixedBottomButton.types.ts
+++ src/components/common/FixedBottomButton/FixedBottomButton.types.ts
@@ -5,11 +5,15 @@ export type Props = {
    * The content to be displayed on the left button.
    */
   left?: React.ReactNode;
-
   /**
    * The content to be displayed on the right button.
    */
   right?: React.ReactNode;
+  /**
+   * Disabled state of the button.
+   * @default 'false'
+   */
+  disabled?: boolean;
   /**
    * Optional children for the button, typically a string.
    */
