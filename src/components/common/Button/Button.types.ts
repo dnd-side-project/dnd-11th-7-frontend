7f55ce74365247e9306c23658ad0d2d8d5e5020b
keemsebeen <sebin0580@gmail.com>
2024-08-08 18:51:12 +0900

    fix: ButtonHTMLAttributes 제거

--- src/components/common/Button/Button.types.ts
+++ src/components/common/Button/Button.types.ts
@@ -1,6 +1,6 @@
-import { ButtonHTMLAttributes, ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef } from 'react';
 
-export type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+export type Props = {
   /**
    * Disabled state of the button.
    * @default 'false'
