e8ec7e7273b8d6dec74890c0e69615cbb0143338
sungjihyun <jhsung23@gmail.com>
2024-08-10 01:09:26 +0900

    feat: Switch 컴포넌트, 스토리북

---
+++ src/components/common/Switch/Switch.styled.ts
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+import { colors } from '@/styles/global';
+
+import { SwitchButtonProps } from './Switch.types';
+
+export const StyeldButton = styled.button<SwitchButtonProps>`
+  display: flex;
+  flex-direction: column;
+  width: 170px;
+  height: 174px;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid ${colors.GY4};
+  border-radius: 16px;
+  background-color: ${colors.WH};
+  padding: 13px 0 34px 0;
+  cursor: pointer;
+`;
---
+++ src/components/common/Switch/index.tsx
@@ -0,0 +1,48 @@
+import { createContext, useContext } from 'react';
+import { css } from '@emotion/react';
+
+import { FlexBox } from '@/components/common/FlexBox';
+import { Icon } from '@/components/common/Icon';
+import { Body3 } from '@/components/common/Typography';
+import { colors } from '@/styles/global';
+
+import { StyeldButton } from './Switch.styled';
+import { Props, SwitchButtonProps } from './Switch.types';
+
+const SwitchContext = createContext({ selectedValue: '', onChange: (value: string) => {} });
+export const Switch = ({ selectedValue, onChange, children }: Props) => {
+  return (
+    <SwitchContext.Provider value={{ selectedValue, onChange }}>
+      <FlexBox
+        flexDir="row"
+        gap={13}
+        css={css`
+          width: 100%;
+        `}
+      >
+        {children}
+      </FlexBox>
+    </SwitchContext.Provider>
+  );
+};
+
+const Button = ({ label, icon }: SwitchButtonProps) => {
+  const { selectedValue, onChange } = useContext(SwitchContext);
+  const isSelected = selectedValue === label;
+
+  return (
+    <StyeldButton
+      label={label}
+      icon={icon}
+      css={css`
+        border-color: ${isSelected ? `${colors.purple}` : `${colors.GY4}`};
+      `}
+      onClick={() => onChange(label)}
+    >
+      <Body3 color={isSelected ? 'purple' : 'GY4'}>{label}</Body3>
+      <Icon name={icon} size={112} color={isSelected ? 'purple' : 'GY4'} />
+    </StyeldButton>
+  );
+};
+
+Switch.Button = Button;
