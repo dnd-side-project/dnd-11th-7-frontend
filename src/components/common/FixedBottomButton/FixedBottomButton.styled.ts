3a7609530886bcf568ca761c76cea0619171cfe0
keemsebeen <sebin0580@gmail.com>
2024-08-11 01:43:06 +0900

    feat: FixedBottomButton Container styled 구현

---
+++ src/components/common/FixedBottomButton/FixedBottomButton.styled.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+import { colors } from '@/styles/global';
+
+export const Container = styled.div`
+  width: 100%;
+  background-color: ${colors.WH};
+  border: none;
+`;
