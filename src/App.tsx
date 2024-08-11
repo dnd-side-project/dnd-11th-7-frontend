130b136e53337e8d82095f4796032ef9a5d381f4
keemsebeen <sebin0580@gmail.com>
2024-08-11 18:45:59 +0900

    feat: Login 페이지 Route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import { Login } from './pages/login';
+import { Login } from './pages/Login';
 
 export const App = () => {
   return (
