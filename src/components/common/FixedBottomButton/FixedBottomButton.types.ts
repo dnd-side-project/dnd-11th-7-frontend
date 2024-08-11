1143dce2c68c08d9f18a1e3f87d0536204386552
keemsebeen <sebin0580@gmail.com>
2024-08-11 15:13:16 +0900

    fix: FixedBottomButton type 유니언으로 수정

--- src/components/common/FixedBottomButton/FixedBottomButton.types.ts
+++ src/components/common/FixedBottomButton/FixedBottomButton.types.ts
@@ -1,14 +1,14 @@
 import { ComponentPropsWithoutRef } from 'react';
 
-export type Props = {
+export type SingleButtonProps = {
   /**
    * The content to be displayed on the left button.
    */
-  left?: never | React.ReactNode;
+  left?: never;
   /**
    * The content to be displayed on the right button.
    */
-  right?: never | React.ReactNode;
+  right?: never;
   /**
    * Disabled state of the button.
    * @default 'false'
@@ -17,5 +17,27 @@ export type Props = {
   /**
    * Optional children for the button, typically a string.
    */
-  children?: never | React.ReactNode;
+  children: React.ReactNode;
 } & ComponentPropsWithoutRef<'button'>;
+
+export type DualButtonProps = {
+  /**
+   * The content to be displayed on the left button.
+   */
+  left: React.ReactNode;
+  /**
+   * The content to be displayed on the right button.
+   */
+  right: React.ReactNode;
+  /**
+   * Disabled state of the button.
+   * @default 'false'
+   */
+  disabled?: boolean;
+  /**
+   * Optional children for the button, typically a string.
+   */
+  children?: never;
+};
+
+export type Props = SingleButtonProps | DualButtonProps;
