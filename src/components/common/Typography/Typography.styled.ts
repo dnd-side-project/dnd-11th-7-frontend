3cae040d1b82e72c30a576425c1f443d92bdf1af
sungjihyun <jhsung23@gmail.com>
2024-08-11 17:43:50 +0900

    design: prewrap

--- src/components/common/Typography/Typography.styled.ts
+++ src/components/common/Typography/Typography.styled.ts
@@ -8,46 +8,55 @@ const typographyStyles = {
     font-size: 32px;
     font-weight: ${weights.semibold};
     line-height: 140%;
+    white-space: pre-wrap;
   `,
   Head2: css`
     font-size: 28px;
     font-weight: ${weights.semibold};
     line-height: 140%;
+    white-space: pre-wrap;
   `,
   Head3: css`
     font-size: 24px;
     font-weight: ${weights.semibold};
     line-height: 140%;
+    white-space: pre-wrap;
   `,
   Head4: css`
     font-size: 22px;
     font-weight: ${weights.semibold};
     line-height: 140%;
+    white-space: pre-wrap;
   `,
   Body1: css`
     font-size: 20px;
     font-weight: ${weights.semibold};
     line-height: 140%;
+    white-space: pre-wrap;
   `,
   Body2: css`
     font-size: 18px;
     font-weight: ${weights.semibold};
     line-height: 140%;
+    white-space: pre-wrap;
   `,
   Body3: css`
     font-size: 16px;
     font-weight: ${weights.semibold};
     line-height: 140%;
+    white-space: pre-wrap;
   `,
   Body4: css`
     font-size: 14px;
     font-weight: ${weights.semibold};
     line-height: 140%;
+    white-space: pre-wrap;
   `,
   caption: css`
     font-size: 12px;
     font-weight: ${weights.semibold};
     line-height: 140%;
+    white-space: pre-wrap;
     display: inline-block;
   `,
 };
