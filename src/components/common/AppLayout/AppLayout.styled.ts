e6d627d45e5ea33da880f2afe8c1748d99555c6c
sungjihyun <jhsung23@gmail.com>
2024-08-11 17:26:20 +0900

    fix: max-width

--- src/components/common/AppLayout/AppLayout.styled.ts
+++ src/components/common/AppLayout/AppLayout.styled.ts
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 import { colors } from '@/styles/global';
 
 export const StyledAppLayout = styled.div`
-  width: 393px;
+  max-width: 430px;
   height: 100vh;
   margin: auto;
   background-color: ${colors.GY6};
