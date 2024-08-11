d5331fc5206dd9a4aca0b07842a1965e8fcdca4e
keemsebeen <sebin0580@gmail.com>
2024-08-11 21:29:52 +0900

    feat: KakaoLoginButton styled 설정

---
+++ src/components/features/KakaoLoginButton/KakaoLoginButton.styled.ts
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const KakaoButtonContainer = styled.button`
+  width: 100%;
+  height: 50px;
+  padding: 13px 17px;
+  border: none;
+  border-radius: 12px;
+  background-color: #fee500;
+  font-size: 16px;
+  font-weight: 500;
+  cursor: pointer;
+  outline: none;
+`;
