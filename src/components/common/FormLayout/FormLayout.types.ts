80c8c5dbed4120e2062668157f7e57037a02e517
Sung Ji Hyun <jhsung23@gmail.com>
2024-08-12 23:38:14 +0900

    [FEAT] FormLayout 컴포넌트, 스토리북 (#73)

---
+++ src/components/common/FormLayout/FormLayout.types.ts
@@ -0,0 +1,7 @@
+export type Props = {
+  title: string;
+  description?: string;
+  progressValue: number;
+  progressMaxValue?: number;
+  content: React.ReactNode;
+};
---
+++ src/components/common/FormLayout/index.tsx
@@ -0,0 +1,43 @@
+import { css } from '@emotion/react';
+
+import { Chip } from '@/components/common/Chip';
+import { FlexBox } from '@/components/common/FlexBox';
+import { Header } from '@/components/common/Header';
+import { IconButton } from '@/components/common/IconButton';
+import { Progress } from '@/components/common/Progress';
+import { Caption, Head3 } from '@/components/common/Typography';
+
+import { Props } from './FormLayout.types';
+
+export const FormLayout = ({
+  title,
+  description,
+  progressValue,
+  progressMaxValue = 6,
+  content,
+}: Props) => {
+  return (
+    <FlexBox justifyContent="flex-start" height="100%">
+      <Header
+        left={<IconButton iconName="back" />}
+        middle={<Progress min={1} max={progressMaxValue} value={progressValue} />}
+        right={<Chip variant="greyFilled">{`${progressValue}/${progressMaxValue}`}</Chip>}
+      />
+      <FlexBox
+        width="100%"
+        alignItems="flex-start"
+        gap={12}
+        padding="20px 20px 100px 20px"
+        css={css`
+          overflow-y: scroll;
+        `}
+      >
+        <Head3>{title}</Head3>
+        <Caption regularWeight color="GY3">
+          {description}
+        </Caption>
+        {content}
+      </FlexBox>
+    </FlexBox>
+  );
+};
