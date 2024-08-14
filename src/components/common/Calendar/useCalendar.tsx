c61026ed17de1a426b4a1a5828de8cec82f6d409
keemsebeen <sebin0580@gmail.com>
2024-08-15 01:24:19 +0900

    refact: 주석삭제, 중첩 if문 수정

--- src/components/common/Calendar/useCalendar.tsx
+++ src/components/common/Calendar/useCalendar.tsx
@@ -9,11 +9,10 @@ export const useCalendar = ({ startDate, endDate, onDateChange }: Props) => {
   const today = dayjs().startOf('day');
   const currentMonthStart = dayjs().startOf('month');
 
-  // 이전 달, 다음 달로 선택
   const handleDateChange = (direction: string) => {
     setCurrentMonth((prev) => {
       const newMonth = direction === 'prev' ? prev.subtract(1, 'month') : prev.add(1, 'month');
-      // 현재 달보다 이전으로는 이동 불가
+
       return newMonth.isBefore(currentMonthStart) ? currentMonthStart : newMonth;
     });
   };
@@ -25,36 +24,40 @@ export const useCalendar = ({ startDate, endDate, onDateChange }: Props) => {
 
     if (!startDate) {
       onDateChange(date, null);
-    } else if (startDate && !endDate) {
-      if (date.isSame(startDate, 'day')) {
-        onDateChange(null, null);
-      } else if (date.isAfter(startDate)) {
-        const dayDifference = date.diff(startDate, 'day');
-        if (dayDifference <= 14) {
-          onDateChange(startDate, date);
-        } else {
-          onDateChange(startDate, startDate.add(13, 'day'));
-        }
-      } else {
-        onDateChange(date, startDate);
-      }
-    } else if (startDate && endDate) {
-      if (date.isSame(endDate, 'day')) {
-        onDateChange(startDate, null);
-      } else {
-        onDateChange(date, null);
-      }
+      return;
     }
+
+    if (endDate) {
+      onDateChange(date.isSame(endDate, 'day') ? startDate : date, null);
+      return;
+    }
+
+    if (date.isSame(startDate, 'day')) {
+      onDateChange(null, null);
+      return;
+    }
+
+    if (!date.isAfter(startDate)) {
+      onDateChange(date, startDate);
+      return;
+    }
+
+    if (date.diff(startDate, 'day') <= 14) {
+      onDateChange(startDate, date);
+      return;
+    }
+
+    onDateChange(startDate, startDate.add(13, 'day'));
   };
 
   const generateDates = () => {
     const dates = [];
-    const startOfMonth = currentMonth.startOf('month'); // 현재 달의 시작일
-    const startDayOfWeek = startOfMonth.day(); // 현재 달의 시작 요일
+    const startOfMonth = currentMonth.startOf('month');
+    const startDayOfWeek = startOfMonth.day();
 
     for (let i = 0; i < 35; i++) {
-      const currentDate = startOfMonth.add(i - startDayOfWeek, 'day'); // 현재 달의 날짜
-      const isCurrentMonth = currentDate.isSame(currentMonth, 'month'); // 현재 달인지 확인
+      const currentDate = startOfMonth.add(i - startDayOfWeek, 'day');
+      const isCurrentMonth = currentDate.isSame(currentMonth, 'month');
 
       const isSelected =
         currentDate.isSame(startDate, 'day') || (endDate && currentDate.isSame(endDate, 'day')); // 선택된 날짜인지 확인
@@ -62,13 +65,13 @@ export const useCalendar = ({ startDate, endDate, onDateChange }: Props) => {
         startDate &&
         endDate &&
         currentDate.isAfter(startDate, 'day') &&
-        currentDate.isBefore(endDate, 'day'); // 선택된 범위인지 확인
-      const isRangeStart = startDate && currentDate.isSame(startDate, 'day'); // 선택된 범위의 시작일인지 확인
-      const isRangeEnd = endDate && currentDate.isSame(endDate, 'day'); // 선택된 범위의 마지막일인지 확인
+        currentDate.isBefore(endDate, 'day');
+      const isRangeStart = startDate && currentDate.isSame(startDate, 'day');
+      const isRangeEnd = endDate && currentDate.isSame(endDate, 'day');
       const isDisabled =
         currentDate.isBefore(today) ||
         currentDate.isAfter(today.add(30, 'day')) ||
-        (startDate && currentDate.isAfter(startDate.add(14, 'day'))); // 선택 불가능한 날짜인지 확인
+        (startDate && currentDate.isAfter(startDate.add(14, 'day')));
 
       dates.push({
         date: currentDate,
