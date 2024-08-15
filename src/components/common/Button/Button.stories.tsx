8db78f090f56978427fc979c62db7ca335144e7e
keemsebeen <sebin0580@gmail.com>
2024-08-16 00:30:17 +0900

    feat: Button storybook 예시 추가

--- src/components/common/Button/Button.stories.tsx
+++ src/components/common/Button/Button.stories.tsx
@@ -14,19 +14,88 @@ export default meta;
 type Story = StoryObj<typeof Button>;
 
 export const Basic: Story = {
-  args: {
-    disabled: false,
-  },
-  argTypes: {
-    disabled: {
-      control: { type: 'boolean' },
-    },
-  },
-  render: (args) => (
-    <FlexBox flexDir="column" gap={8} alignItems="center">
-      <Button {...args}>버튼</Button>
-      <Button {...args} disabled>
-        버튼
+  render: () => (
+    <>
+      <FlexBox flexDir="row" gap={8} alignItems="center" width="60%">
+        <FlexBox
+          flexDir="column"
+          gap={8}
+          justifyContent="space-between"
+          alignItems="center"
+          width="70%"
+        >
+          <Button variant="big">다음</Button>
+          <Button variant="big" disabled>
+            다음
+          </Button>
+        </FlexBox>
+        <FlexBox
+          flexDir="column"
+          gap={8}
+          justifyContent="space-between"
+          alignItems="center"
+          width="30%"
+        >
+          <Button variant="grey">돌아가기</Button>
+          <Button variant="grey" disabled>
+            돌아가기
+          </Button>
+        </FlexBox>
+      </FlexBox>
+      <FlexBox flexDir="column" gap={8} alignItems="center" width="60%" margin="10px 0 0 0">
+        <Button variant="p2" height={55}>
+          전체보기
+        </Button>
+        <Button variant="p2" height={55} disabled>
+          전체보기
+        </Button>
+      </FlexBox>
+      <FlexBox flexDir="row" gap={8} alignItems="center" width="60%" margin="10px 0 0 0">
+        <FlexBox flexDir="column" gap={8} alignItems="center" width="100%">
+          <Button variant="grey" height={50}>
+            비회원으로 시작하기
+          </Button>
+          <Button variant="grey" height={50} disabled>
+            비회원으로 시작하기
+          </Button>
+        </FlexBox>
+      </FlexBox>
+    </>
+  ),
+};
+
+export const Big: Story = {
+  render: () => (
+    <FlexBox flexDir="column" gap={8} alignItems="center" width="60%">
+      <Button variant="big">다음</Button>
+      <Button variant="big" disabled>
+        다음
+      </Button>
+    </FlexBox>
+  ),
+};
+
+export const Grey: Story = {
+  render: () => (
+    <FlexBox flexDir="column" gap={8} alignItems="center" width="60%">
+      <Button variant="grey" height={55}>
+        돌아가기
+      </Button>
+      <Button variant="grey" height={55} disabled>
+        돌아가기
+      </Button>
+    </FlexBox>
+  ),
+};
+
+export const P2: Story = {
+  render: () => (
+    <FlexBox flexDir="column" gap={8} alignItems="center" width="60%">
+      <Button variant="p2" height={50}>
+        전체보기
+      </Button>
+      <Button variant="p2" height={50} disabled>
+        전체보기
       </Button>
     </FlexBox>
   ),
