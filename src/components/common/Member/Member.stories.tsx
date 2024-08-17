5d38b7a0cfb5b0627f4676697996ad98d3ad6c82
keemsebeen <sebin0580@gmail.com>
2024-08-17 18:38:16 +0900

    fix: Member storybook 설정 수정

--- src/components/common/Member/Member.stories.tsx
+++ src/components/common/Member/Member.stories.tsx
@@ -16,6 +16,27 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof Member>;
 
+export const Basic: Story = {
+  args: {
+    isData: true,
+    isChecked: true,
+    isLeader: false,
+    isAnonymous: false,
+    anonymousNumber: 1,
+    JJakkakEmoji: 'jjakkak1',
+    children: '리더',
+  },
+  render: (args) => {
+    return (
+      <FlexBox flexDir="row" flexWrap="wrap" justifyContent="flex-start" gap={10}>
+        <Member {...args} JJakkakEmoji={<Icon name={args.JJakkakEmoji as IconName} size={27} />}>
+          {args.children}
+        </Member>
+      </FlexBox>
+    );
+  },
+};
+
 const memberVariations: Array<{
   isData: boolean;
   isChecked?: boolean;
@@ -24,14 +45,14 @@ const memberVariations: Array<{
   name: string;
 }> = [
   { isData: true, isLeader: true, emoji: 'jjakkak1', name: '리더' },
-  { isData: true, isChecked: true, emoji: 'jjakkak2', name: '승조' },
+  { isData: true, emoji: 'jjakkak2', name: '승조' },
   { isData: true, emoji: 'jjakkak3', name: '태웅' },
   { isData: false, emoji: 'jjakkak4', name: '세빈' },
   { isData: false, emoji: 'jjakkak5', name: '지현' },
   { isData: false, emoji: 'jjakkak6', name: '채영' },
   { isData: false, emoji: 'jjakkak7', name: '노트북 두고 거제도 놀러간 경욱공주' },
 ];
-export const Basic: Story = {
+export const Member_Display: Story = {
   render: () => {
     return (
       <FlexBox flexDir="row" flexWrap="wrap" justifyContent="flex-start" gap={10}>
