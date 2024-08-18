2d7b46bd85d7defa39151a056c514d7aa5dd37b7
keemsebeen <sebin0580@gmail.com>
2024-08-18 22:17:59 +0900

    feat : Modal storybook 설정

---
+++ src/components/common/Modal/Modal.stories.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable no-restricted-exports */
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { useModal } from './useModal';
+
+import { Modal } from '.';
+import { AppLayout } from '../AppLayout';
+import { Button } from '../Button';
+import { FlexBox } from '../FlexBox';
+import { Icon } from '../Icon';
+
+const meta = {
+  title: 'components/common/Modal',
+  component: Modal,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+type Story = StoryObj<typeof Modal>;
+
+export const Basic: Story = {
+  args: {
+    showCloseButton: true,
+  },
+  argTypes: {
+    showCloseButton: {
+      control: 'boolean',
+    },
+  },
+  render: (args) => {
+    const { isOpen, openModal, closeModal, modalRef } = useModal();
+
+    return (
+      <AppLayout>
+        <FlexBox
+          flexDir="column"
+          justifyContent="center"
+          alignItems="center"
+          gap={10}
+          width="50%"
+          height="100vh"
+          margin="auto"
+        >
+          <Button onClick={openModal}>모달</Button>
+
+          <Modal
+            isOpen={isOpen}
+            onClose={closeModal}
+            title="DND 7팀 회의"
+            modalRef={modalRef}
+            showCloseButton={args.showCloseButton}
+          >
+            <FlexBox flexDir="column" gap={10}>
+              <Icon name="jjakkak1" size={40} />
+              <Button onClick={closeModal}>확인</Button>
+            </FlexBox>
+          </Modal>
+        </FlexBox>
+      </AppLayout>
+    );
+  },
+};
