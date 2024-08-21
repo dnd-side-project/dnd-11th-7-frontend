b03dc28ed0051c6caef591e5ed4c39ef8b5f9d18
keemsebeen <sebin0580@gmail.com>
2024-08-22 00:40:40 +0900

    fix: Button type 수정

--- src/components/common/Button/Button.types.ts
+++ src/components/common/Button/Button.types.ts
@@ -3,25 +3,14 @@ import { ComponentPropsWithoutRef } from 'react';
 export type Props = {
   /**
    * The variant of the button.
-   * @default 'CTN'
    */
-  variant: 'CTN' | 'grey' | 'box';
+  variant: 'primary' | 'secondary' | 'tertiary';
   /**
    * The height of the button.
    */
-  height?: number;
-  /**
-   * Disabled state of the button.
-   * @default 'false'
-   */
-  disabled?: boolean;
-  /**
-   * fontSize of the button.
-   * @default 'large'
-   */
-  fontSize?: 'small' | 'medium' | 'large';
+  height: 'large' | 'medium' | 'small';
   /**
    * Optional children for the button, typically a string.
    */
-  children?: React.ReactNode;
+  children: React.ReactNode;
 } & ComponentPropsWithoutRef<'button'>;
