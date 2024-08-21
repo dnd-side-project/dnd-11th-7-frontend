8d6c67af5128070c85925dcd080adedc92cf283e
sungjihyun <jhsung23@gmail.com>
2024-08-22 00:58:15 +0900

    feat: 카테고리 선택 폼

--- src/components/features/CreateMeetingForm/CategoryForm.tsx
+++ src/components/features/CreateMeetingForm/CategoryForm.tsx
@@ -1,39 +1,54 @@
 import { useState } from 'react';
-import { css } from '@emotion/react';
 
 import { Chip } from '@/components/common/Chip';
 import { FixedBottomButton } from '@/components/common/FixedBottomButton';
 import { FlexBox } from '@/components/common/FlexBox';
 import { FormLayout } from '@/components/common/FormLayout';
+import { categories } from '@/constants/meetingForm';
 import { useFunnelProgressContext } from '@/hooks/useFunnelProgressContext';
 
 import { CreateMeetingFormBaseProps } from './types';
 
 type Props = CreateMeetingFormBaseProps;
 
-const categories = ['학교', '친구', '팀플', '회의', '스터디', '취미', '봉사', '기타']; // TODO 임시 데이터
+const MIN_SELECT_COUNT = 1;
+const MAX_SELECT_COUNT = 3;
+
 export const CategoryForm = ({ onNext, onPrev }: Props) => {
   const { progress, maxProgress } = useFunnelProgressContext();
-  const [selectedChip, setSelectedChip] = useState<string[]>([]); // TODO 임시 state
+  const [selectedCategory, setSelectedCategory] = useState<string[]>([]);
+
+  const canClick = (chipValue: string) => {
+    return (
+      (!selectedCategory.includes(chipValue) && selectedCategory.length < MAX_SELECT_COUNT) ||
+      (selectedCategory.includes(chipValue) && selectedCategory.length === MAX_SELECT_COUNT) ||
+      selectedCategory.length < MAX_SELECT_COUNT
+    );
+  };
+
+  const handleClickChip = (chipValue: string) => {
+    if (!canClick(chipValue)) return;
+    return selectedCategory.includes(chipValue)
+      ? setSelectedCategory(selectedCategory.filter((selected) => selected !== chipValue))
+      : setSelectedCategory([...selectedCategory, chipValue]);
+  };
 
   return (
     <>
       <FormLayout
         header={<FormLayout.Header progress={progress} maxProgress={maxProgress} onPrev={onPrev} />}
         title={`카테고리를\n선택해 주세요`}
-        description="최대 3개까지 선택할 수 있습니다."
+        description={`최대 ${MAX_SELECT_COUNT}개까지 선택할 수 있습니다.`}
         content={
           <FlexBox width="100%" padding="60px 60px 0">
-            <Chip.Group
-              css={css`
-                width: 60%;
-              `}
-            >
+            <Chip.Group>
               {categories.map((category) => (
                 <Chip
+                  key={category}
                   component="button"
-                  variant={selectedChip.includes(category) ? 'filled' : 'dimmed'}
-                  onClick={() => setSelectedChip((prev) => [...prev, category])}
+                  variant={selectedCategory.includes(category) ? 'filled' : 'dimmed'}
+                  onClick={() => handleClickChip(category)}
+                  disabled={!canClick(category)}
                 >
                   {category}
                 </Chip>
@@ -42,7 +57,12 @@ export const CategoryForm = ({ onNext, onPrev }: Props) => {
           </FlexBox>
         }
       />
-      <FixedBottomButton onClick={onNext}>다음</FixedBottomButton>
+      <FixedBottomButton
+        disabled={selectedCategory.length < MIN_SELECT_COUNT}
+        onClick={() => onNext(selectedCategory)}
+      >
+        다음
+      </FixedBottomButton>
     </>
   );
 };
