57baf928ddf889e8f83661d14ee50ff795fe4f7d
sungjihyun <jhsung23@gmail.com>
2024-08-23 00:54:34 +0900

    feat: 일정 입력 마감 기한 폼

--- src/components/features/CreateMeetingForm/DeadlineForm.tsx
+++ src/components/features/CreateMeetingForm/DeadlineForm.tsx
@@ -1,5 +1,9 @@
+import { useState } from 'react';
+
 import { FixedBottomButton } from '@/components/common/FixedBottomButton';
+import { FlexBox } from '@/components/common/FlexBox';
 import { FormLayout } from '@/components/common/FormLayout';
+import { Switch } from '@/components/common/Switch';
 import { useFunnelProgressContext } from '@/hooks/useFunnelProgressContext';
 
 import { CreateMeetingFormBaseProps } from './types';
@@ -8,13 +12,23 @@ type Props = CreateMeetingFormBaseProps;
 
 export const DeadlineForm = ({ onNext, onPrev }: Props) => {
   const { progress, maxProgress } = useFunnelProgressContext();
+  const [selectedDeadline, setSelectedDeadline] = useState('오늘'); // TODO 인터페이스 정의 필요
 
   return (
     <>
       <FormLayout
         header={<FormLayout.Header progress={progress} maxProgress={maxProgress} onPrev={onPrev} />}
         title={`일정 입력 마감 기한을\n기입해 주세요`}
-        content="마감 기한 입력 영역"
+        content={
+          <FlexBox width="100%" padding="78px 0">
+            <Switch selectedValue={selectedDeadline} onChange={setSelectedDeadline}>
+              <Switch.Button label="오늘" value="오늘" />
+              <Switch.Button label="내일" value="내일" />
+              <Switch.Button label="3일" value="3일" />
+              <Switch.Button label="5일" value="5일" />
+            </Switch>
+          </FlexBox>
+        }
       />
       <FixedBottomButton onClick={onNext}>다음</FixedBottomButton>
     </>
