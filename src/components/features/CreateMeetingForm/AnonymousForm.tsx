866bfd170d68b13bf79939d64cba9abe7622792a
sungjihyun <jhsung23@gmail.com>
2024-08-23 00:53:20 +0900

    feat: 익명 여부 폼

--- src/components/features/CreateMeetingForm/AnonymousForm.tsx
+++ src/components/features/CreateMeetingForm/AnonymousForm.tsx
@@ -1,5 +1,9 @@
+import { useState } from 'react';
+
 import { FixedBottomButton } from '@/components/common/FixedBottomButton';
+import { FlexBox } from '@/components/common/FlexBox';
 import { FormLayout } from '@/components/common/FormLayout';
+import { Switch } from '@/components/common/Switch';
 import { useFunnelProgressContext } from '@/hooks/useFunnelProgressContext';
 
 import { CreateMeetingFormBaseProps } from './types';
@@ -8,6 +12,7 @@ type Props = CreateMeetingFormBaseProps;
 
 export const AnonymousForm = ({ onNext, onPrev }: Props) => {
   const { progress, maxProgress } = useFunnelProgressContext();
+  const [isAnonymous, setIsAnonymous] = useState(false);
 
   return (
     <>
@@ -15,7 +20,22 @@ export const AnonymousForm = ({ onNext, onPrev }: Props) => {
         header={<FormLayout.Header progress={progress} maxProgress={maxProgress} onPrev={onPrev} />}
         title={`멤버들의 일정을\n어떻게 수집할까요?`}
         description="익명 여부를 선택해 주세요"
-        content="세그먼티드 컨트롤 영역"
+        content={
+          <FlexBox width="100%" padding="60px 0">
+            <Switch selectedValue={isAnonymous} onChange={setIsAnonymous}>
+              <Switch.Button
+                label="실명"
+                value={false}
+                img={<img src="/images/btn-jjakkak-3.svg" />}
+              />
+              <Switch.Button
+                label="익명"
+                value={true}
+                img={<img src="/images/btn-anonymous.svg" />}
+              />
+            </Switch>
+          </FlexBox>
+        }
       />
       <FixedBottomButton onClick={onNext}>다음</FixedBottomButton>
     </>
