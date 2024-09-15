c1d629f19e0b2bac4731d8db59da35fca2d2ea98
keemsebeen <sebin0580@gmail.com>
2024-09-15 23:45:42 +0900

    feat : schedulecard 스토리북, 컴포넌트 구현

---
+++ src/components/features/ScheduleCard/index.tsx
@@ -0,0 +1,48 @@
+import { forwardRef } from 'react';
+import { css } from '@emotion/react';
+
+import { StyledMemberCardContainer, StyledMemberInfoContainer } from './ScheduleCard.styled';
+import { Props } from './ScheduleCard.types';
+
+import { Chip } from '../../common/Chip/index';
+import { FlexBox } from '../../common/FlexBox/index';
+import { Caption, Body2 } from '../../common/Typography/index';
+
+export const ScheduleCard = forwardRef<HTMLDivElement, Props>(
+  ({ attendeeCount, dateTime, attendees = [], variant = 'default', ...props }, ref) => {
+    return (
+      <StyledMemberCardContainer ref={ref} variant={variant} {...props}>
+        <FlexBox justifyContent="center" alignItems="flex-start">
+          <Caption
+            regularWeight
+            color={variant === 'default' ? 'GY3' : 'WH'}
+            css={css`
+              margin-bottom: 4px;
+            `}
+          >
+            {attendeeCount}
+          </Caption>
+          <Body2
+            color={variant === 'default' ? 'purple' : 'WH'}
+            css={css`
+              margin-bottom: ${attendees && '6px'};
+            `}
+          >
+            {dateTime}
+          </Body2>
+          <StyledMemberInfoContainer>
+            {attendees.length > 0 && (
+              <Chip.Group type="slide">
+                {attendees.map((item, index) => (
+                  <Chip key={index} variant={variant === 'default' ? 'primary' : 'secondary'}>
+                    {item}
+                  </Chip>
+                ))}
+              </Chip.Group>
+            )}
+          </StyledMemberInfoContainer>
+        </FlexBox>
+      </StyledMemberCardContainer>
+    );
+  }
+);
