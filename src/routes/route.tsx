c1aadcf97f6d1d814d3eaa5384f461ca486667bd
sungjihyun <jhsung23@gmail.com>
2024-09-15 17:37:58 +0900

    feat: 로그인 관련 페이지 라우트

--- src/routes/route.tsx
+++ src/routes/route.tsx
@@ -2,7 +2,11 @@ import { createBrowserRouter } from 'react-router-dom';
 
 import { App } from '@/App';
 import { Login } from '@/pages/Login';
+import { LoginFailure } from '@/pages/LoginFailure';
+import { LoginSuccess } from '@/pages/LoginSuccess';
 import { NewMeeting } from '@/pages/NewMeeting';
+
+import { PrivateRoute } from './PrivateRoute';
 
 export const router = createBrowserRouter([
   {
@@ -11,14 +15,22 @@ export const router = createBrowserRouter([
     children: [
       {
         path: 'login',
-        element: <Login />,
+        children: [
+          { index: true, element: <Login /> },
+          { path: 'success', element: <LoginSuccess /> },
+          { path: 'failure', element: <LoginFailure /> },
+        ],
       },
       {
         path: 'meeting',
         children: [
           {
             path: 'new',
-            element: <NewMeeting />,
+            element: (
+              <PrivateRoute>
+                <NewMeeting />
+              </PrivateRoute>
+            ),
           },
         ],
       },
