852cab0c04cdc6fbe2909e47a9ddc83165ec82db
sungjihyun <jhsung23@gmail.com>
2024-09-17 01:43:15 +0900

    feat: 일정 마감 기한 유효성 검사

--- src/components/features/CreateMeetingForm/types.ts
+++ src/components/features/CreateMeetingForm/types.ts
@@ -4,5 +4,6 @@ export type CreateMeetingFormBaseProps = {
 };
 
 export type FormData<T> = {
-  context: { state: T; setState: (newState: T) => void };
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  context: { state: T; setState: (newState: T) => void; others?: any };
 };
