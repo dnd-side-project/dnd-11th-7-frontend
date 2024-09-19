b907688c502e179df0859d5dfafafc47bb53c5ec
keemsebeen <sebin0580@gmail.com>
2024-09-19 15:30:51 +0900

    feat: 일정 입력 페이지 구현

---
+++ src/pages/NewSchedule.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+import { newScheduleStepNames } from '@/constants/scheduleFrom';
+import { useFunnel } from '@/hooks/useFunnel';
+import { Schedule, newSchedule } from '@/types/schedule';
+
+import { NickNameForm } from '../components/features/CreateScheduleForm/NickNameForm';
+import { ScheduleInputForm } from '../components/features/CreateScheduleForm/ScheduleInputForm';
+
+export const NewSchedule = () => {
+  const { Funnel, setStep } = useFunnel(newScheduleStepNames);
+
+  const [newSchedule, setNewSchedule] = useState<newSchedule>({
+    dateOfScheduleList: [],
+    nickName: '',
+  });
+
+  const updateSchedule = (key: keyof newSchedule, value: string | Schedule[]) => {
+    setNewSchedule((prev) => ({ ...prev, [key]: value }));
+  };
+
+  return (
+    <Funnel>
+      <Funnel.Step name="닉네임설정">
+        <NickNameForm
+          onPrev={() => {}}
+          onNext={() => setStep('일정입력')}
+          value={newSchedule.nickName}
+          onUpdateNickName={(value: string) => updateSchedule('nickName', value)}
+        />
+      </Funnel.Step>
+
+      <Funnel.Step name="일정입력">
+        <ScheduleInputForm
+          onPrev={() => setStep('닉네임설정')}
+          onNext={() => {}}
+          onUpdateSchedule={(value: Schedule[]) => updateSchedule('dateOfScheduleList', value)}
+        />
+      </Funnel.Step>
+    </Funnel>
+  );
+};
