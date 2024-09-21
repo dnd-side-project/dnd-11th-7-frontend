c113c4b66a61cdd14b3f3b9406793b60ed1e12de
sungjihyun <jhsung23@gmail.com>
2024-09-21 22:08:45 +0900

    feat: 내비게이션 연결

--- src/pages/Meeting.tsx
+++ src/pages/Meeting.tsx
@@ -1,4 +1,5 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 
 import { queries } from '@/apis';
 import { FixedBottomButton } from '@/components/common/FixedBottomButton';
@@ -8,6 +9,7 @@ import { Icon } from '@/components/common/Icon';
 import { MeetingList } from '@/components/features/MeetingList';
 
 export const Meeting = () => {
+  const navigate = useNavigate();
   const { data } = useSuspenseQuery(queries.member.meetings); // TODO 에러처리
   const isDataEmpty = data.length === 0;
 
@@ -16,9 +18,11 @@ export const Meeting = () => {
       <Header left={<Icon name="jjakkakText" size={{ width: 115, height: 13 }} color="BK" />} />
       <MeetingList title="나의 모임" data={data} />
       {isDataEmpty ? (
-        <FixedBottomButton>모임 일정 생성하기</FixedBottomButton>
+        <FixedBottomButton onClick={() => navigate('/meeting/new')}>
+          모임 일정 생성하기
+        </FixedBottomButton>
       ) : (
-        <FloatingActionButton />
+        <FloatingActionButton onClick={() => navigate('/meeting/new')} />
       )}
     </>
   );
