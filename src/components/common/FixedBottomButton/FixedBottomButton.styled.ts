23f888f71cd194e823217611d1d72f96c53dd6d8
keemsebeen <sebin0580@gmail.com>
2024-09-21 21:37:44 +0900

    fix: css 수정

--- src/components/common/FixedBottomButton/FixedBottomButton.styled.ts
+++ src/components/common/FixedBottomButton/FixedBottomButton.styled.ts
@@ -3,8 +3,6 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
   position: fixed;
   bottom: 0;
-  left: 0;
-  right: 0;
   width: 100%;
   max-width: 430px;
   padding-left: 20px;
