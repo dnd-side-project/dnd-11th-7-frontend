a11bc6c690f97e2a4224b813bbcc314c460e7529
keemsebeen <sebin0580@gmail.com>
2024-09-21 21:42:54 +0900

    feat: ScheduleLayout Header backgroundColor 지정

--- src/components/common/ScheduleLayout/ScheduleLayout.styled.ts
+++ src/components/common/ScheduleLayout/ScheduleLayout.styled.ts
@@ -8,7 +8,8 @@ export const StyledScheduleLayoutContainer = styled.div`
   justify-content: flex-start;
   background: linear-gradient(
     180deg,
-    rgba(133, 123, 255, 0.9) 0%,
+    rgba(133, 123, 255) 0%,
+    rgba(133, 123, 255) 10%,
     rgba(133, 123, 255, 0.9) 60%,
     ${colors.GY6} 100%
   );
