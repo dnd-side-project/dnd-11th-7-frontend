a336a21a0b11ad44190dd669b0b269bff7984cc2
sungjihyun <jhsung23@gmail.com>
2024-09-21 22:08:20 +0900

    feat: api 연결

--- src/pages/Meeting.tsx
+++ src/pages/Meeting.tsx
@@ -1,3 +1,6 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+import { queries } from '@/apis';
 import { FixedBottomButton } from '@/components/common/FixedBottomButton';
 import { FloatingActionButton } from '@/components/common/FloatingActionButton';
 import { Header } from '@/components/common/Header';
@@ -5,12 +8,13 @@ import { Icon } from '@/components/common/Icon';
 import { MeetingList } from '@/components/features/MeetingList';
 
 export const Meeting = () => {
-  const isDataEmpty = [].length === 0; // TODO data
+  const { data } = useSuspenseQuery(queries.member.meetings); // TODO 에러처리
+  const isDataEmpty = data.length === 0;
 
   return (
     <>
       <Header left={<Icon name="jjakkakText" size={{ width: 115, height: 13 }} color="BK" />} />
-      <MeetingList title="나의 모임" data={[]} />
+      <MeetingList title="나의 모임" data={data} />
       {isDataEmpty ? (
         <FixedBottomButton>모임 일정 생성하기</FixedBottomButton>
       ) : (
