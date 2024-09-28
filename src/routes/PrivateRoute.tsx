55936c578aff7ad386ecc8b81123c767ca31811a
sungjihyun <jhsung23@gmail.com>
2024-09-28 09:19:28 +0900

    feat: 접근 예외적 허용, 로그인 후 리다이렉트 적용

--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,4 +1,4 @@
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 
 import { useAuth } from '@/hooks/useAuth';
 
@@ -7,11 +7,17 @@ type Props = {
 };
 
 export const PrivateRoute = ({ children }: Props) => {
+  const { pathname, state } = useLocation();
   const { isAuthenticated } = useAuth();
+  const isAccessAllowed = state === 'allow'; // NOTE: accessToken이 없어도 예외적으로 접근을 허용할 때 사용 (e.g. 비회원으로 일정 입력하기)
 
-  if (!isAuthenticated) {
+  if (!isAuthenticated && !isAccessAllowed) {
+    /**
+     * 로그인하지 않았을 때 권한을 필요로 하는 페이지(children으로 렌더링할 페이지)에 접근하면,
+     * 로그인을 마치고 다시 권한을 필요로 하는 페이지로 이동하도록 현재 페이지(children으로 렌더링할 페이지)의 pathname을 전달
+     */
     console.error('접근할 수 없는 페이지입니다.');
-    return <Navigate to="/login" />;
+    return <Navigate to={`/login?redirect=${pathname}`} />;
   }
 
   return <>{children}</>;
