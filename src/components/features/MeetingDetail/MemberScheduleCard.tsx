6c5f9eb6d2637ef783f76c12fb8ddee553eee92d
keemsebeen <108217858+keemsebin@users.noreply.github.com>
2024-10-08 23:13:49 +0900

    [FEAT] best-time api 연결 (#241)

--- src/components/features/MeetingDetail/MemberScheduleCard.tsx
+++ src/components/features/MeetingDetail/MemberScheduleCard.tsx
@@ -12,27 +12,25 @@ import { UuidProps, NavigateProps } from './MeetingDetail.type';
 export type MemberScheduleProps = UuidProps & NavigateProps;
 
 export const MemberScheduleCard = ({ uuid, onNavigate }: MemberScheduleProps) => {
-  const { data: scheduleTimeData } = useSuspenseQuery(queries.meeting.times(uuid));
+  const { data: bestTime } = useSuspenseQuery(queries.meeting.bestTime(uuid));
 
   return (
     <FlexBox width="100%" padding="30px 20px 10px 20px">
       <Card emojiPosition="top-right">
         <FlexBox alignItems="normal" gap={6}>
-          {scheduleTimeData.meetingTimeList.length === 0 ? (
+          {bestTime ? (
+            <>
+              <Body3 regularWeight color="GY3">
+                가장 많은 선택을 받은 날짜는
+              </Body3>
+              <Head3 color="purple">{formatDateTime(bestTime)}</Head3>
+            </>
+          ) : (
             <Body2
               css={css`
                 text-align: center;
               `}
             >{`아직 일정을 입력한 사람이 없어요.\n일정을 입력해주세요!`}</Body2>
-          ) : (
-            <>
-              <Body3 regularWeight color="GY3">
-                가장 많은 선택을 받은 날짜는
-              </Body3>
-              <Head3 color="purple">
-                {formatDateTime(scheduleTimeData.meetingTimeList[0].startTime)}
-              </Head3>
-            </>
           )}
         </FlexBox>
         <FlexBox flexDir="row" gap={10} padding="20px 0 0 0">
@@ -40,11 +38,11 @@ export const MemberScheduleCard = ({ uuid, onNavigate }: MemberScheduleProps) =>
             일정 입력하기
           </Button>
           <Button
-            variant={scheduleTimeData.meetingTimeList.length === 0 ? 'tertiary' : 'primary'}
+            variant={bestTime ? 'tertiary' : 'primary'}
             height="medium"
-            disabled={scheduleTimeData.meetingTimeList.length === 0}
+            disabled={!bestTime}
             onClick={() => {
-              if (scheduleTimeData.meetingTimeList.length !== 0) {
+              if (bestTime) {
                 onNavigate(`/${uuid}/schedules`);
               }
             }}
