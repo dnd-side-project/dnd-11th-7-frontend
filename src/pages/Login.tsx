538b8091091957ecbe383f2da8892edd762f62ff
sungjihyun <jhsung23@gmail.com>
2024-10-10 22:28:39 +0900

    fix

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,6 +6,7 @@ import { Icon } from '@/components/common/Icon';
 import { Head3 } from '@/components/common/Typography';
 import { KakaoLoginButton } from '@/components/features/KakaoLoginButton';
 import { ENV } from '@/lib/env';
+import { isUnauthorizedUserAccessibleUrlRegExp } from '@/utils/regex';
 
 export const Login = () => {
   const [searchParams] = useSearchParams();
@@ -19,7 +20,7 @@ export const Login = () => {
         <Head3>{`모임의 시작을\n더욱 간편하게`}</Head3>
       </FlexBox>
       <FlexBox width="100%" gap={12}>
-        {is비회원접근가능Url(redirect) && (
+        {isUnauthorizedUserAccessibleUrl(redirect) && (
           <Button
             variant="tertiary"
             height="small"
@@ -34,13 +35,5 @@ export const Login = () => {
   );
 };
 
-/**
- * NOTE: 비회원도 접근 가능한 페이지지만 로그인 유도를 위해 Login 페이지로 리다이렉트됐을 때,
- * `비회원으로 시작하기` 버튼 노출이 필요함
- *
- * 로그인 완료 후 리다이렉트할 url이 비회원 허용 페이지인지 확인하기 위해 url을 정규식으로 검사함
- * 비회원 허용 페이지에는 [/:uuid/new (일정 입력), ...]가 있음
- */
-const 비회원접근가능UrlRegExp = [/^\/[A-Za-z\d]{8}\/new$/];
-const is비회원접근가능Url = (url: string) =>
-  비회원접근가능UrlRegExp.some((regex) => regex.test(url));
+const isUnauthorizedUserAccessibleUrl = (url: string) =>
+  isUnauthorizedUserAccessibleUrlRegExp.some((regex) => regex.test(url));
