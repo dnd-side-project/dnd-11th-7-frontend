16de415049f57fc8e8c7b90f16c82be941de30aa
keemsebeen <sebin0580@gmail.com>
2024-11-29 02:10:22 +0900

    feat : Loading 컴포넌트 구현

---
+++ src/components/common/Loading/index.tsx
@@ -0,0 +1,13 @@
+import Lottie from 'react-lottie';
+
+import LoadingJson from './loading.json';
+
+export const Loading = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: LoadingJson,
+  };
+
+  return <Lottie options={defaultOptions} height={150} width={150} />;
+};
